80426d9671bdcb422ad2cb0c4640fc356dd35c0a
mairasglitch <mairarizvi345@gmail.com>
2025-02-17 21:41:28 +0530

    Advanced result middleware

--- DevcamperAPI/src/route/bootcamp.js
+++ DevcamperAPI/src/route/bootcamp.js
@@ -10,6 +10,9 @@ const {
   bootcampPhotoUpload,
 } = require("../controllers/bootcamps");
 
+const Bootcamp = require("../models/bootcamp");
+const advancedResults = require("../middleware/advancedResults");
+
 //Include other resource routers
 const courseRouter = require("./course");
 
@@ -22,7 +25,10 @@ router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 router.route("/:id/photo").put(bootcampPhotoUpload);
 
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route("/:id")
